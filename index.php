0401690c11097b923af51566a58fa04278705faa
limse11 <g00glecansuckmypp@gmail.com>
2022-11-22 17:50:26 +0100

    .

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="./css/master.css">
+    <script src="./js/login.js" defer></script>
   </head>
   <body>
     <?php
@@ -11,5 +13,24 @@
       $password = "";
       $db = "baza";
     ?>
+    <div class="lform">
+      <button type="button" class="lform-b" onclick="log()">Logowanie</button>
+      <button type="button" class="lform-b" onclick="rej()">Rejestracja</button>
+      <div class="labs">
+        <div id="log">
+          <h1>Logowanie</h1>
+          <input type="text" name="login" placeholder="Login" id="login"><br>
+          <input type="password" name="pass" placeholder="Hasło" id="pass"><br>
+          <input type="submit" value="Zaloguj">
+        </div>
+        <div id="rej">
+          <h1>Rejestracja</h1>
+          <input type="text" name="rlogin" placeholder="Login" id="rlogin"><br>
+          <input type="email" name="email" placeholder="E-mail"><br>
+          <input type="password" name="rpass" placeholder="Hasło" id="rpass"><br>
+          <input type="submit" value="Zarejestruj">
+        </div>
+      </div>
+    </div>
   </body>
 </html>
---
+++ js/login.js
@@ -0,0 +1,8 @@
+function log(){
+  document.querySelector('#log').style.display = 'block';
+  document.querySelector('#rej').style.display = 'none';
+}
+function rej(){
+  document.querySelector('#rej').style.display = 'block';
+  document.querySelector('#log').style.display = 'none';
+}
